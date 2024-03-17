710d8d402d67da31df604d226234b1195a185ae1
Fauzi Pratma <fauzipratama11@gmail.com>
2024-03-17 11:43:30 +0700

    Update Env Using User Secret

--- apps/Program.cs
+++ apps/Program.cs
@@ -2,42 +2,32 @@
 using apps.Helper;
 using apps.Configs;
 using apps.Services;
-using apps.Models.Contexts;
 using System.Reflection;
+using StackExchange.Redis;
+using apps.Models.Contexts;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using VaultSharp.Extensions.Configuration;
-using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Config Vault
-VaultConfig vaultConfig = builder.Configuration.GetSection("Vault").Get<VaultConfig>() ?? 
-    throw new ArgumentNullException("Env Vault Config is null");
-
-if(vaultConfig is not { Link : not null, BasePath : not null, Token: not null, MountPoint: not null })
-    throw new ArgumentNullException("Env Vault Config is null or not complete !!");
-
-builder.Configuration.AddJsonFile("appsettings.json").AddVaultConfiguration(() =>
-    new VaultOptions(vaultAddress: vaultConfig.Link, vaultToken: vaultConfig.Token), vaultConfig.BasePath, vaultConfig.MountPoint);
+VaultConfig vaultConfig = builder.Configuration.GetSection("Vault").Get<VaultConfig>()!;
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddVaultConfiguration(() =>
+        new VaultOptions(vaultAddress: vaultConfig.Link, vaultToken: vaultConfig.Token), vaultConfig.BasePath, vaultConfig.MountPoint);
 
 //Config Env
 builder.Services.Configure<AppConfig>(builder.Configuration);
-AppConfig appConfig = builder.Configuration.Get<AppConfig>() ?? 
-    throw new ArgumentNullException($"Env Apps is null on Vault path {vaultConfig.MountPoint}{vaultConfig.BasePath}");
+AppConfig appConfig = builder.Configuration.Get<AppConfig>()!;
 
 //Config Db PostgreSql
-if(appConfig.PostgreSqlConnectionString is null)
-    throw new ArgumentNullException("PostgreSqlConnectionString");
-
 builder.Services.AddDbContext<DataDbContext>(options => {
     options.UseNpgsql(appConfig.PostgreSqlConnectionString!);
 });
 
 //Config Redis
-if(appConfig.RedisConnectionString is null)
-    throw new ArgumentNullException("RedisConnectionString");
-
 builder.Services.AddSingleton<IDatabase>(provider =>
 {
     var redisConfig = ConfigurationOptions.Parse(appConfig!.RedisConnectionString!);
